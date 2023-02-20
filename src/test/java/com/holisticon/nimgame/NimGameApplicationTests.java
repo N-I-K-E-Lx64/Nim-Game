540a15da8555fa416617bbb7c950a734136e1a3e
Niklas Schünemann <coding.nikel@gmail.com>
2023-02-20 12:46:28 +0100

    Initial commit

---
+++ src/test/java/com/holisticon/nimgame/NimGameApplicationTests.java
@@ -0,0 +1,13 @@
+package com.holisticon.nimgame;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NimGameApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
